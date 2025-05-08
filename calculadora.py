75d1a5749fb5cd7ba917b2c00a0261a2287d0b22
Maria Eugenia <mariaeu.alvarez@gmail.com>
2025-05-08 14:28:09 +0200

    añado calculadora python

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+a=float(input("escribe el primer número:"))
+b=float(input("escribe el segundo número:"))
+
+print("la suma es igual a,", a+b)
+print("la resta es igual a,", a-b)
+
+print("la multiplicación es igual a,", a*b)
+
+if b!=0:
+
+    print("la división es igual a,", a/b)
+
+else:
+
+    print("no se puede dividir")
